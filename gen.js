ef9ae58bdec34804f1b9b62d0d764a998b828e70
yapcheahshen <yapcheahshen@gmail.com>
2017-03-19 13:28:55 +0800

    add wrong xml sample

--- gen.js
+++ gen.js
@@ -1,10 +1,7 @@
-var createCorpusFromJSON=null;
-try{
-	createCorpusFromJSON=require("ksana-corpus-builder").createCorpusFromJSON;
-} catch(e){
-	createCorpusFromJSON=require("ksana-corpus-lib").createCorpusFromJSON;
-}
+const lib="ksana-corpus-builder";
+const createCorpusFromJSON=require(lib).createCorpusFromJSON;
 
-createCorpusFromJSON("xml/manual-corpus.json",function(err){
+createCorpusFromJSON("xml/manual-corpus.json",function(err,bytewritten){
 	if (err)console.log(err);
+	else console.log(bytewritten);
 });
