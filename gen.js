35154fafa7bfa25ce8eae01e8d9005cc0d922692
yapcheahshen <yapcheahshen@gmail.com>
2017-04-13 21:18:08 +0800

    use ksana-corpus-lib

--- gen.js
+++ gen.js
@@ -1,6 +1,4 @@
-const lib="ksana-corpus-builder";
-const createCorpusFromJSON=require(lib).createCorpusFromJSON;
-
+const {createCorpusFromJSON}=require("ksana-corpus-lib");
 createCorpusFromJSON("xml/manual-corpus.json",function(err,bytewritten){
 	if (err)console.log(err);
 	else console.log(bytewritten);
