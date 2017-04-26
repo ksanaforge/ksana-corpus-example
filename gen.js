0fe7f06e0c89568d27a1445eba521e55e777a092
yapcheahshen <yapcheahshen@gmail.com>
2017-04-26 10:51:34 +0800

    sample for external fields

--- gen.js
+++ gen.js
@@ -1,4 +1,10 @@
-const {createCorpusFromJSON}=require("ksana-corpus-builder");
+var createCorpusFromJSON=null
+try {
+	createCorpusFromJSON=require("ksana-corpus-builder").createCorpusFromJSON;
+} catch(e){
+	createCorpusFromJSON=require("ksana-corpus-lib").createCorpusFromJSON;
+}
+
 createCorpusFromJSON("xml/manual-corpus.json",function(err,bytewritten){
 	if (err)console.log(err);
 	else console.log(bytewritten);
